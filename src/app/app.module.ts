db8b29494ce4aebd8b7486288db713e2b0a9344f
Balint Horvath <horvath.balint997@gmail.com>
2023-01-31 13:05:25 +0100

    firebase init and deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBMxDf_A0-iOfO1CUm9sI-Rk7KPgbQ-RRA',
+  authDomain: 'tasktick-52e91.firebaseapp.com',
+  projectId: 'tasktick-52e91',
+  storageBucket: 'tasktick-52e91.appspot.com',
+  messagingSenderId: '772996813588',
+  appId: '1:772996813588:web:ae79404638e2e24e6f2801',
+  measurementId: 'G-L3DK5P5F1J',
+};
+
+initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [AppComponent, TodolistComponent, LoginComponent],
   imports: [
